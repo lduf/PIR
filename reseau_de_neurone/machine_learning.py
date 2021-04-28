6adf379d6ed2b47bf3c1741022ac8fa82407b5ed
lduf <lucas.dufour@insa-lyon.fr>
2021-04-28 23:08:48 +0200

    je fais du ménage pcq ça bug

--- reseau_de_neurone/machine_learning.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:edbad804fe87f5d82c251146cf4e5173f41db79d7e384385303910f0b2945ca5
-size 4540
--- reseau_de_neurone/mushroom-learning.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:db1a1efb788227c5a9147ba3b0ad83603ffad2094a28e96793f700bd5e97c622
-size 5594
