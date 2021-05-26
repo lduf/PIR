5fa23caad5e0815c28687f67943908b8e94a5ce1
lduf <lucas.dufour@insa-lyon.fr>
2021-05-26 14:19:13 +0200

    ajout de la prédiction depuis un dossier

--- reseau_de_neurone/predict.py
+++ reseau_de_neurone/predict.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb3244bfeb792472f14b0f8794b88184647a9a87058cefb2a5ee57d14d1e5e4a
-size 1096
+oid sha256:49a4bbd9158e6da09b9616928b2e8386d59559414d68d9e26b3c0da2753e534f
+size 3389
